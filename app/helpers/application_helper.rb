0cf1c1a002156ce9959f99e958bd30dd1fb8b29a
Jason Post <jason.w.post@gmail.com>
2015-10-29 20:07:07 +1300

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	# Returns the full title on a per-page basis.
+	def full_title(page_title = '')
+		base_title = "Jason Post - Composer, Sound Artist, Developer"
+		if page_title.empty? #boolean check
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
 end
